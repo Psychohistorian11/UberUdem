5657053a4593be26087aee0c4036690fb6eb8a94
Psychohistorian <cristianfrancoraigosa@gmail.com>
2024-02-10 14:30:38 -0500

    primer commit rama Cristian

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 let map;
 let marker;
 let markerDestiny;
+let directionsService;
+let directionDisplay;
 
 function initMap() {
-    // Configurar el mapa inicial, el que mira el usuario sin haber registrado
-    // ninguna dirección
+    // Configurar el mapa inicial
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 6.2442, lng: -75.5812 },
         zoom: 10,
     });
 
+    directionsService = new google.maps.DirectionsService();
+    directionDisplay = new google.maps.DirectionsRenderer();
+
+    directionDisplay.setMap(map);
+
     // Inicializar el marcador
     marker = new google.maps.Marker({
         map: map,
-        draggable: true, 
+        draggable: true,
     });
 
-     // Inicializar el marcador de destino
-     markerDestiny = new google.maps.Marker({
+    // Inicializar el marcador de destino
+    markerDestiny = new google.maps.Marker({
         map: map,
-        draggable: true, 
+        draggable: true,
     });
-   
+
     google.maps.event.addListener(marker, 'dragend', function (event) {
         const lat = marker.getPosition().lat();
         const lng = marker.getPosition().lng();
@@ -32,6 +38,10 @@ function initMap() {
         const lat = markerDestiny.getPosition().lat();
         const lng = markerDestiny.getPosition().lng();
         console.log("Nuevas coordenadas Destino:", lat, lng);
+    });
+
+    google.maps.event.addListenerOnce(map, 'idle', () => {
+        document.getElementById("map").classList.add('show-map');
     });
 }
 
@@ -49,25 +59,43 @@ function buscarRuta() {
 
             marker.setPosition({ lat: lat, lng: lng });
 
+            const destino = document.getElementById("destino").value;
+
+            const geocoderDestiny = new google.maps.Geocoder();
+            geocoderDestiny.geocode({ address: destino }, function (results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    const latDestiny = results[0].geometry.location.lat();
+                    const lngDestiny = results[0].geometry.location.lng();
+
+                    markerDestiny.setPosition({ lat: latDestiny, lng: lngDestiny });
+
+                    calculateRoute(); // Llamada a calculateRoute después de obtener las coordenadas de destino
+                } else {
+                    console.error("Error al obtener las coordenadas:", status);
+                }
+            });
         } else {
             console.error("Error al obtener las coordenadas:", status);
         }
     });
+}
 
-
-    const destino = document.getElementById("destino").value;
-
-    const geocoderDestiny = new google.maps.Geocoder();
-    geocoderDestiny.geocode({address: destino}, function(results, status){
-        if (status === google.maps.GeocoderStatus.OK){
-
-            const latDestiny = results[0].geometry.location.lat();
-            const lngDestiny = results[0].geometry.location.lng();
-
-            markerDestiny.setPosition({ lat: latDestiny, lng: lngDestiny });
-
-        }else {
-            console.error("Error al obtener las coordenadas:", status);
+function calculateRoute() {
+    directionsService.route({
+        origin: {
+            lat: marker.getPosition().lat(),
+            lng: marker.getPosition().lng(),
+        },
+        destination: {
+            lat: markerDestiny.getPosition().lat(),
+            lng: markerDestiny.getPosition().lng(),
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+    }, (response, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+            directionDisplay.setDirections(response);
+        } else {
+            alert('No se pudieron mostrar las direcciones debido a: ' + status);
         }
-    })
+    });
 }
