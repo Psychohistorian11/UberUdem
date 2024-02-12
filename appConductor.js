5e727caa30c48ab7ea927795d3270cc49453a9b4
Psychohistorian <cristianfrancoraigosa@gmail.com>
2024-02-12 09:32:33 -0500

    avance

--- appConductor.js
+++ appConductor.js
@@ -1,9 +1,11 @@
 let map;
+let initialMap;
 let marker;
 let markerDestiny;
 let directionsService;
 let directionDisplay;
 let routeGenerated = false;
+
 
 function initMap() {
 
@@ -13,6 +15,7 @@ function initMap() {
         center: { lat: 6.2442, lng: -75.5812 },
         zoom: 12,
     });
+
 
 
     directionsService = new google.maps.DirectionsService();
@@ -31,27 +34,38 @@ function initMap() {
         map: map,
         draggable: true,
     });
-   
-
-
 
     google.maps.event.addListenerOnce(map, 'idle', () => {
         document.getElementById("map").classList.add('show-map');
         
     });
 
+
     const salida = localStorage.getItem('salida');
     const destino = localStorage.getItem('destino');
+
     if (salida != "" && destino != ""){
-        buscarRuta(salida,destino)
-        buscarConductor()
+        Swal.fire({
+            title: "Pasajero Disponible",
+            icon: "info",
+            showCloseButton: true,
+            showConfirmButton: false,
+            html: "<button class='swal2-cancel swal2-styled' onclick='acceptTravel()'>Aceptar Viaje</button>" +
+                  "<button class='swal2-cancel swal2-styled' onclick='cancelTravel()'>Cancelar Viaje</button>",    
+        });
+        
     }
+}
+
+function acceptTravel(){
 
 }
 
+function cancelTravel(){
+    
+}
+
 function buscarRuta(salidaNew="",destinoNew="") {
-    let salida = document.getElementById("salida").value;
-    let destino = document.getElementById("destino").value;
 
     if(salidaNew != "" && destinoNew != ""){
         salida = salidaNew
@@ -65,7 +79,7 @@ function buscarRuta(salidaNew="",destinoNew="") {
             const lat = results[0].geometry.location.lat();
             const lng = results[0].geometry.location.lng();
 
-            map.setCenter({ lat: lat, lng: lng });
+            initialMap.setCenter({ lat: lat, lng: lng });
             marker.setPosition({ lat: lat, lng: lng });
 
             const geocoderDestiny = new google.maps.Geocoder();
@@ -78,8 +92,8 @@ function buscarRuta(salidaNew="",destinoNew="") {
                     markerDestiny.setPosition({ lat: latDestiny, lng: lngDestiny });
 
 
-                    calculateRoute(); // Llamada a calculateRoute después de obtener las coordenadas de destino
-                    enviarInformacion(salida,destino)
+                    calculateRoute();
+             
                 } else {
                     console.error("Error al obtener las coordenadas:", status);
                     Swal.fire({
@@ -101,7 +115,6 @@ function buscarRuta(salidaNew="",destinoNew="") {
     });
 }
 
-
 function calculateRoute() {
     directionsService.route({
         origin: {
@@ -117,80 +130,8 @@ function calculateRoute() {
         if (status === google.maps.DirectionsStatus.OK) {
             directionDisplay.setDirections(response);
             routeGenerated = true;
-            mostrarBoton();
         } else {
             alert('No se pudieron mostrar las direcciones debido a: ' + status);
         }
     });
 }
-
-function mostrarBoton() {
-    if (routeGenerated) {
-        const contenedorBoton = document.getElementById("botonContainer");
-
-        // Eliminar cualquier botón existente dentro del contenedor
-        contenedorBoton.innerHTML = '';
-
-        // Crear el botón y añadirlo al contenedor
-        const botonBuscarConductor = document.createElement("button");
-        botonBuscarConductor.textContent = "Buscar Conductor";
-        botonBuscarConductor.onclick = buscarConductor;
-        contenedorBoton.appendChild(botonBuscarConductor);
-    }
-}
-
-function buscarConductor() {
-
-    //aquí debe haber algo para la logica del condcutor
-
-    const salida = localStorage.getItem('salida');
-    const destino = localStorage.getItem('destino');
-
-    Swal.fire({
-        title: "Buscando conductor",
-        icon: "info",
-        showCloseButton: true,
-        showConfirmButton: false,
-        html: "<button class='swal2-cancel swal2-styled' onclick='cancelarPedido()'>Cancelar Pedido</button>" +
-              "<button class='swal2-cancel swal2-styled' onclick='menuClicked()'>Ir al Menú</button>",    
-    });
-}
-
-
-function cancelarPedido() {
-    // Limpiar las marcas y la ruta
-    marker.setPosition(null);
-    markerDestiny.setPosition(null);
-    directionDisplay.setDirections({ routes: [] });
-    routeGenerated = false;
-
-    // Mostrar el mapa predeterminado
-    map.setCenter({ lat: 6.2442, lng: -75.5812 });
-    map.setZoom(12);
-
-    // Ocultar el contenedor del botón
-    const contenedorBoton = document.getElementById("botonContainer");
-    contenedorBoton.innerHTML = '';
-
-    // Limpiar el localStorage
-    localStorage.removeItem('salida');
-    localStorage.removeItem('destino');
-
-    // Cerrar el cuadro de diálogo de SweetAlert
-    Swal.close();
-
-}
-
-function menuClicked() {
-    // Puedes agregar aquí el código que se ejecutará cuando se haga clic en el botón "Menu"
-    // Por ejemplo, puedes abrir un menú o realizar otra acción relacionada con el menú.
-    window.location.href = "index.html";
-}
-
-function enviarInformacion(salida, destino){
-    localStorage.setItem('salida', salida);
-    localStorage.setItem('destino', destino);
-
-}
-
-
